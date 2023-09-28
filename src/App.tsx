b8b1f6baa3848be3b65ca338734aa9334c88e4b1
Alexotieno1717 <alexotieno900@gmail.com>
2023-09-28 17:15:52 +0300

    :sparkles: feat: finish set up tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="bg-primary-dark-blue">
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className="text-primary-purple">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>
@@ -37,11 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-purple": "#C778DD",
+        "primary-dark-blue" : "#282C33"
+      },
+    },
   },
   plugins: [],
 };
