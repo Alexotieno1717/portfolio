6ccba6fe5b410eca4983dcb29a378c6f2d7ecd06
Alexotieno1717 <alexotieno900@gmail.com>
2023-10-13 10:21:21 +0300

    :sparkles: feat: created contacts section component and added contact routing

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home';
 import About from './pages/about';
 import Project from './pages/project';
+import Contacts from './pages/contacts';
 
 const AppRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const AppRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<About />} />
       <Route path="/works" element={<Project />} />
+      <Route path="/contacts" element={<Contacts />} />
       <Route path="*" element="Page does not exist" />
     </Routes>
   );
