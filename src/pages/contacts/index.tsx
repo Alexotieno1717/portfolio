3f8697c97ed1731c282f85519c393711517f36cd
Alexotieno1717 <alexotieno900@gmail.com>
2023-10-13 12:15:08 +0300

    :sparkles: feat: Added content for contact and also styled the page

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,5 +1,15 @@
+import Footer from '../../components/navigation/Footer';
+import Header from '../../components/navigation/Header';
+import ContactSection from '../../components/sections/contacts/ContactSection';
+
 const Contacts = () => {
-  return <div>Contacts</div>;
+  return (
+    <div>
+      <Header />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
 };
 
 export default Contacts;
